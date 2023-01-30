a2e694ce3d57495e6dab198ddcaba29791fea258
Scarlett Yeung <ymw.scarlett@gmail.com>
2023-01-30 16:44:31 +0800

    updated event page css

--- project-app/src/pages/Events/Events.tsx
+++ project-app/src/pages/Events/Events.tsx
@@ -2,11 +2,12 @@ import { Button } from '@mantine/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BadgeCard } from './components/EventCard';
+import '../../styles/event.css';
 
 function Events() {
 	return (
 		<div className='Body'>
-			<div className='Title Header'>
+			<div className='Title Header' id='EventHeader'>
 				Events
 				<Link to={`/events/createEvents`}>
 					<Button radius='md' style={{ flex: 1 }}>
