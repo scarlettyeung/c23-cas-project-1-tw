3a227b4a89af2bdd1413cccc909bb3dc4bdaf9f5
Ken Chan <kenchan6694@gmail.com>
2023-02-04 22:31:24 +0800

    updated display css

--- project-app/src/components/MyRoutes.tsx
+++ project-app/src/components/MyRoutes.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Home from '../pages/Home/Home';
-import ChatRoom from '../pages/Chat/ChatRoom';
+// import ChatRoom from '../pages/Chat/ChatRoom';
 import Events from '../pages/Events/Events';
 import About from '../pages/About';
 import Eprofile from '../pages/Eprofile/Eprofile';
@@ -19,36 +19,36 @@ import PersonalDetails from '../pages/PersonalDetails';
 import LoadHistory from '../pages/History/LoadHistory';
 
 function MyRoutes() {
-	return (
-		<Routes>
-			<Route path='login' element={<Login />} />
-			<Route path='register' element={<PublicRoute />}>
-				<Route element={<Register />} index />
-				<Route path='corporate' element={<Corporate />} />
-				<Route path='individual' element={<Individual />} />
-				<Route path='performer' element={<Performer />} />
-			</Route>
-			<Route path='/' element={<PrivateRoute />}>
-				<Route element={<Home />} index />
-				<Route path='events' element={<Events />} />
-				<Route path='events/:eventsId' element={<EventDetail />} />
-				<Route path='events/createEvents' element={<CreateEventPage />} />
-				<Route path='history/application' element={<LoadHistory />} />
-				<Route path='chatRoom' element={<ChatRoom />} />
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='register' element={<PublicRoute />}>
+        <Route element={<Register />} index />
+        <Route path='corporate' element={<Corporate />} />
+        <Route path='individual' element={<Individual />} />
+        <Route path='performer' element={<Performer />} />
+      </Route>
+      <Route path='/' element={<PrivateRoute />}>
+        <Route element={<Home />} index />
+        <Route path='events' element={<Events />} />
+        <Route path='events/:eventsId' element={<EventDetail />} />
+        <Route path='events/createEvents' element={<CreateEventPage />} />
+        <Route path='history/application' element={<LoadHistory />} />
+        {/* <Route path='chatRoom' element={<ChatRoom />} /> */}
 
-				<Route path='about'>
-					<Route element={<PersonalDetails />} index />
-					<Route path='uuid/:uuid' element={<About />} />
-				</Route>
+        <Route path='about'>
+          <Route element={<PersonalDetails />} index />
+          <Route path='uuid/:uuid' element={<About />} />
+        </Route>
 
-				<Route path='eProfile'>
-					<Route path='uuid/:uuid/get' element={<Eprofile />} index />
-				</Route>
-			</Route>
+        <Route path='eProfile'>
+          <Route path='uuid/:uuid/get' element={<Eprofile />} index />
+        </Route>
+      </Route>
 
-			<Route path='*' element={<NotFound />} />
-		</Routes>
-	);
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default MyRoutes;
