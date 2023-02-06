ed6c030cbe31ecbf95b3491ec750031ba0cd9640
Ken Chan <kenchan6694@gmail.com>
2023-02-06 11:16:16 +0800

    change logo

--- project-app/src/pages/Register/Performer/Performer.tsx
+++ project-app/src/pages/Register/Performer/Performer.tsx
@@ -12,140 +12,139 @@ import { checkHashValidation, checkPswValidation } from '../../../redux/auth/sli
 import '../../../styles/register.css';
 
 export type FormData = {
-	email: string;
-	password: string;
-	username: string;
-	tagId: number[] | null;
-	firstName: string;
-	lastName: string;
-	experience: string;
-	contact: string;
-	birthday: string;
-	description: string;
-	gender: string;
-	facebookURL: string;
-	twitterURL: string;
-	youtubeURL: string;
-	igURL: string;
-	contactEmail: string;
+  email: string;
+  password: string;
+  username: string;
+  tagId: number[] | null;
+  firstName: string;
+  lastName: string;
+  experience: string;
+  contact: string;
+  birthday: string;
+  description: string;
+  gender: string;
+  facebookURL: string;
+  twitterURL: string;
+  youtubeURL: string;
+  igURL: string;
+  contactEmail: string;
 };
 
 const INITIAL_DATA: FormData = {
-	email: '',
-	password: '',
-	username: '',
-	tagId: null,
-	firstName: '',
-	lastName: '',
-	experience: '',
-	contact: '',
-	birthday: '',
-	description: '',
-	gender: '',
-	facebookURL: '',
-	twitterURL: '',
-	youtubeURL: '',
-	igURL: '',
-	contactEmail: '',
+  email: '',
+  password: '',
+  username: '',
+  tagId: null,
+  firstName: '',
+  lastName: '',
+  experience: '',
+  contact: '',
+  birthday: '',
+  description: '',
+  gender: '',
+  facebookURL: '',
+  twitterURL: '',
+  youtubeURL: '',
+  igURL: '',
+  contactEmail: '',
 };
 
 function Performer() {
-	const navigate = useNavigate();
-	const dispatch = useRootDispatch();
-	const typeOfAccount = useRootSelector((state) => state.auth.accountType);
-	console.log('I got you', typeOfAccount);
+  const navigate = useNavigate();
+  const dispatch = useRootDispatch();
+  const typeOfAccount = useRootSelector((state) => state.auth.accountType);
+  console.log('I got you', typeOfAccount);
 
-	const [data, setData] = useState(INITIAL_DATA);
-	console.log(data);
+  const [data, setData] = useState(INITIAL_DATA);
+  console.log(data);
 
-	function updateFields(fields: Partial<FormData>) {
-		setData((prev) => {
-			return { ...prev, ...fields };
-		});
-	}
-	let fetchAccType: string;
-	if (typeOfAccount === 'performer') {
-		fetchAccType = 'performer';
-	} else {
-		fetchAccType = '';
-	}
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  let fetchAccType: string;
+  if (typeOfAccount === 'performer') {
+    fetchAccType = 'performer';
+  } else {
+    fetchAccType = '';
+  }
 
-	// console.log('Got fetch', fetchAccType)
-	const fetchData = {
-		identitySelect: fetchAccType,
-		email: data.email,
-		password: data.password,
-		username: data.username,
-		hashtagArr: data.tagId,
-		name: data.firstName + '/' + data.lastName,
-		yearsOfExp: Number(data.experience),
-		contactNumber: Number(data.contact),
-		contactEmail: data.contactEmail,
-		birthday: new Date(data.birthday),
-		description: data.description,
-		gender: data.gender,
-		facebookURL: data.facebookURL,
-		twitterURL: data.twitterURL,
-		youtubeURL: data.youtubeURL,
-		igURL: data.igURL,
-	};
-	// console.log(fetchData);
+  // console.log('Got fetch', fetchAccType)
+  const fetchData = {
+    identitySelect: fetchAccType,
+    email: data.email,
+    password: data.password,
+    username: data.username,
+    hashtagArr: data.tagId,
+    name: data.firstName + '/' + data.lastName,
+    yearsOfExp: Number(data.experience),
+    contactNumber: Number(data.contact),
+    contactEmail: data.contactEmail,
+    birthday: new Date(data.birthday),
+    description: data.description,
+    gender: data.gender,
+    facebookURL: data.facebookURL,
+    twitterURL: data.twitterURL,
+    youtubeURL: data.youtubeURL,
+    igURL: data.igURL,
+  };
+  // console.log(fetchData);
 
-	dispatch(checkPswValidation(data.password));
-	dispatch(checkHashValidation(data.tagId));
+  dispatch(checkPswValidation(data.password));
+  dispatch(checkHashValidation(data.tagId));
 
-	const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultiStepForm([
-		<BasicInfo {...data} updateFields={updateFields} />,
-		<PerformerHash {...data} updateFields={updateFields} />,
-		<PerformerInfo {...data} updateFields={updateFields} />,
-	]);
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultiStepForm([
+    <BasicInfo {...data} updateFields={updateFields} />,
+    <PerformerHash {...data} updateFields={updateFields} />,
+    <PerformerInfo {...data} updateFields={updateFields} />,
+  ]);
 
-	async function onSubmit(e: FormEvent) {
-		e.preventDefault();
-		if (!isLastStep) return next();
-		const path = process.env.REACT_APP_API_BASE;
-		await fetch(`${path}users/createUser`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(fetchData),
-		});
-		navigate('/');
-	}
-
-	return (
-		<div>
-			<form onSubmit={onSubmit} className='register-performer-outerDiv'>
-				<div style={{ marginTop: 70, fontSize: 30, fontWeight: 'bold', marginBottom: 20 }}>
-					Performer Registration
-				</div>
-				{step}
-				<div
-					style={{
-						marginTop: '3vh',
-						marginBottom: '10vh',
-					}}
-				>
-					<div className='register-user-backPageSubmit'>
-						{!isFirstStep ? (
-							<Button className='register-user-back' type='button' onClick={back}>
-								Back
-							</Button>
-						) : (
-							<Button className='register-back-none'>Back</Button>
-						)}
-						<a className='register-performer-pageShow'>
-							{currentStepIndex + 1} / {steps.length}
-						</a>
-						<Button className='register-user-nextAndSubmit' type='submit'>
-							{isLastStep ? 'Submit' : 'Next'}
-						</Button>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+  async function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    const path = process.env.REACT_APP_API_BASE;
+    await fetch(`${path}users/createUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(fetchData),
+    });
+    navigate('/');
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='register-performer-outerDiv'>
+        <div style={{ marginTop: 70, fontSize: 30, fontWeight: 'bold', marginBottom: 20 }}>
+          Performer Registration
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: '3vh',
+            marginBottom: '10vh',
+          }}
+        >
+          <div className='register-user-backPageSubmit'>
+            {!isFirstStep ? (
+              <Button className='register-user-back' type='button' onClick={back}>
+                Back
+              </Button>
+            ) : (
+              <Button className='register-back-none' type='button' onClick={() => navigate("/register")}>Back</Button>
+            )}
+            <a className='register-performer-pageShow'>
+              {currentStepIndex + 1} / {steps.length}
+            </a>
+            <Button className='register-user-nextAndSubmit' type='submit'>
+              {isLastStep ? 'Submit' : 'Next'}
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Performer;
--- project-app/src/pages/Register/corporate/Corporate.tsx
+++ project-app/src/pages/Register/corporate/Corporate.tsx
@@ -8,118 +8,118 @@ import { checkPswValidation } from '../../../redux/auth/slice';
 import { Button } from '@mantine/core';
 
 type FormData = {
-	email: string;
-	password: string;
-	password2: string;
-	username: string;
-	companyName: string;
-	contact: string;
-	businessAddress: string;
-	bRNumber: string;
-	website: string;
-	description: string;
-	gender: string;
-	contactEmail: string;
+  email: string;
+  password: string;
+  password2: string;
+  username: string;
+  companyName: string;
+  contact: string;
+  businessAddress: string;
+  bRNumber: string;
+  website: string;
+  description: string;
+  gender: string;
+  contactEmail: string;
 };
 
 const INITIAL_DATA: FormData = {
-	email: '',
-	password: '',
-	password2: '',
-	username: '',
-	companyName: '',
-	contact: '',
-	businessAddress: '',
-	bRNumber: '',
-	website: '',
-	description: '',
-	gender: '',
-	contactEmail: '',
+  email: '',
+  password: '',
+  password2: '',
+  username: '',
+  companyName: '',
+  contact: '',
+  businessAddress: '',
+  bRNumber: '',
+  website: '',
+  description: '',
+  gender: '',
+  contactEmail: '',
 };
 
 function Corporate() {
-	const navigate = useNavigate();
-	const dispatch = useRootDispatch();
-	const typeOfAccount = useRootSelector((state) => state.auth.accountType);
+  const navigate = useNavigate();
+  const dispatch = useRootDispatch();
+  const typeOfAccount = useRootSelector((state) => state.auth.accountType);
 
-	const [data, setData] = useState(INITIAL_DATA);
-	function updateFields(fields: Partial<FormData>) {
-		setData((prev) => {
-			return { ...prev, ...fields };
-		});
-	}
-	let fetchAccType: string;
-	if (typeOfAccount === 'corporate') {
-		fetchAccType = 'client';
-	} else {
-		fetchAccType = '';
-	}
-	const fetchData = {
-		identitySelect: fetchAccType,
-		clientType: typeOfAccount,
-		email: data.email,
-		password: data.password,
-		username: data.username,
-		name: data.companyName,
-		contactNumber: Number(data.contact),
-		contactEmail: data.contactEmail,
-		businessBRNo: data.bRNumber,
-		businessWebsiteUrl: data.website,
-		description: data.description,
-		gender: data.gender,
-		businessAddress: data.businessAddress,
-	};
-	dispatch(checkPswValidation(data.password));
-	const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultiStepForm([
-		<BasicInfo {...data} updateFields={updateFields} />,
-		<CorporateInfo {...data} updateFields={updateFields} />,
-	]);
-	async function onSubmit(e: FormEvent) {
-		e.preventDefault();
-		if (!isLastStep) return next();
-		const path = process.env.REACT_APP_API_BASE;
-		await fetch(`${path}users/createUser`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(fetchData),
-		});
-		navigate('/');
-	}
+  const [data, setData] = useState(INITIAL_DATA);
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  let fetchAccType: string;
+  if (typeOfAccount === 'corporate') {
+    fetchAccType = 'client';
+  } else {
+    fetchAccType = '';
+  }
+  const fetchData = {
+    identitySelect: fetchAccType,
+    clientType: typeOfAccount,
+    email: data.email,
+    password: data.password,
+    username: data.username,
+    name: data.companyName,
+    contactNumber: Number(data.contact),
+    contactEmail: data.contactEmail,
+    businessBRNo: data.bRNumber,
+    businessWebsiteUrl: data.website,
+    description: data.description,
+    gender: data.gender,
+    businessAddress: data.businessAddress,
+  };
+  dispatch(checkPswValidation(data.password));
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultiStepForm([
+    <BasicInfo {...data} updateFields={updateFields} />,
+    <CorporateInfo {...data} updateFields={updateFields} />,
+  ]);
+  async function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    const path = process.env.REACT_APP_API_BASE;
+    await fetch(`${path}users/createUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(fetchData),
+    });
+    navigate('/');
+  }
 
-	return (
-		<div>
-			<form onSubmit={onSubmit} className='register-performer-outerDiv'>
-				<div style={{ marginTop: 70, fontSize: 30, fontWeight: 'bold', marginBottom: 20 }}>
-					Corporate Client Registration
-				</div>
-				{step}
-				<div
-					style={{
-						marginTop: '3vh',
-						marginBottom: '10vh',
-					}}
-				>
-					<div className='register-user-backPageSubmit'>
-						{!isFirstStep ? (
-							<Button className='register-user-back' type='button' onClick={back}>
-								Back
-							</Button>
-						) : (
-							<Button className='register-back-none'>Back</Button>
-						)}
-						<a className='register-performer-pageShow'>
-							{currentStepIndex + 1} / {steps.length}
-						</a>
-						<Button className='register-user-nextAndSubmit' type='submit'>
-							{isLastStep ? 'Submit' : 'Next'}
-						</Button>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='register-performer-outerDiv'>
+        <div style={{ marginTop: 70, fontSize: 30, fontWeight: 'bold', marginBottom: 20 }}>
+          Corporate Client Registration
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: '3vh',
+            marginBottom: '10vh',
+          }}
+        >
+          <div className='register-user-backPageSubmit'>
+            {!isFirstStep ? (
+              <Button className='register-user-back' type='button' onClick={back}>
+                Back
+              </Button>
+            ) : (
+              <Button className='register-back-none' type='button' onClick={() => navigate("/register")}>Back</Button>
+            )}
+            <a className='register-performer-pageShow'>
+              {currentStepIndex + 1} / {steps.length}
+            </a>
+            <Button className='register-user-nextAndSubmit' type='submit'>
+              {isLastStep ? 'Submit' : 'Next'}
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Corporate;
