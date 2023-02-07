60686f233feb742a6fa5450207e71e52f2804b40
Ken Chan <kenchan6694@gmail.com>
2023-02-07 23:39:42 +0800

    css updated

--- project-app/src/pages/NotFound.tsx
+++ project-app/src/pages/NotFound.tsx
@@ -1,9 +1,10 @@
 function NotFound() {
-	return (
-		<div>
-			<h1>404 Not Found</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <img src="../../public/404.jpeg" alt=""></img>
+    </div>
+  );
 }
 
 export default NotFound;
