c967429417cc32ac6438f129ee4e1c3baa120ea5
Ken Chan <kenchan6694@gmail.com>
2023-02-07 12:22:08 +0800

    updated docker

--- project-app/src/pages/About/components/HashTags.tsx
+++ project-app/src/pages/About/components/HashTags.tsx
@@ -1,32 +1,32 @@
-import { Badge, Group, Title, Flex } from '@mantine/core';
+import { Badge, Title, Flex } from '@mantine/core';
 import { HashTag } from '../../../utils/userInfoType';
 
 type HashTagsProps = {
-	tags: HashTag[] | null;
+  tags: HashTag[] | null;
 };
 function HashTags(props: HashTagsProps) {
-	return (
-		<>
-			{/* <Group position='center' spacing='lg'> */}
-			<div className='EventHistory'>
-				<Title align='left' order={4}>
-					HASH TAGS
-				</Title>
+  return (
+    <>
+      {/* <Group position='center' spacing='lg'> */}
+      <div className='EventHistory'>
+        <Title align='left' order={4}>
+          HASH TAGS
+        </Title>
 
-				<Flex mih={50} gap='xs' justify='flex-start' align='flex-start' direction='row' wrap='wrap'>
-					{props.tags &&
-						props.tags.map((tag) => (
-							<div className='HashTagsKey' key={`userTag_${tag.id}`}>
-								<Badge size='sm' sx={{ paddingLeft: 0 }} radius='xl' key={`tag_${tag.id}`}>
-									{tag.name}
-								</Badge>
-							</div>
-						))}
-				</Flex>
-			</div>
-			{/* </Group> */}
-		</>
-	);
+        <Flex mih={50} gap='xs' justify='flex-start' align='flex-start' direction='row' wrap='wrap'>
+          {props.tags &&
+            props.tags.map((tag) => (
+              <div className='HashTagsKey' key={`userTag_${tag.id}`}>
+                <Badge size='sm' sx={{ paddingLeft: 0 }} radius='xl' key={`tag_${tag.id}`}>
+                  {tag.name}
+                </Badge>
+              </div>
+            ))}
+        </Flex>
+      </div>
+      {/* </Group> */}
+    </>
+  );
 }
 
 export default HashTags;
--- project-app/src/pages/About/index.tsx
+++ project-app/src/pages/About/index.tsx
@@ -1,98 +1,97 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { Button, ActionIcon } from '@mantine/core';
 import useFetch from '../../hooks/useFetch';
 import PersonalInfo from './components/PersonalInfo ';
 import { useRootSelector } from '../../redux/store';
 import {
-	Role,
-	RespTypeInProfile,
-	PerformanceInfo,
-	IndividualClientsInfo,
-	CorporateClientsInfo,
+  Role,
+  RespTypeInProfile,
+  PerformanceInfo,
+  IndividualClientsInfo,
+  CorporateClientsInfo,
 } from '../../utils/userInfoType';
 import '../../styles/about.css';
 import { useEffect, useState } from 'react';
-import { IconBrandWhatsapp } from '@tabler/icons';
+import logger from 'redux-logger';
 
 function About() {
-	const navigate = useNavigate();
-	const { uuid } = useParams<string>()!;
-	const {
-		data: resp,
-		isLoading,
-		error,
-	} = useFetch<RespTypeInProfile | null>(`users/getInfo/${uuid}`, 'GET', null, uuid);
-	const [wsLink, setWSLink] = useState<string>('');
-	const openURL = (url: string) => {
-		window.location.href = url;
-	};
+  const navigate = useNavigate();
+  const { uuid } = useParams<string>()!;
+  const {
+    data: resp,
+    isLoading,
+    error,
+  } = useFetch<RespTypeInProfile | null>(`users/getInfo/${uuid}`, 'GET', null, uuid);
+  const [wsLink, setWSLink] = useState<string>('');
+  const openURL = (url: string) => {
+    window.location.href = url;
+  };
 
-	useEffect(() => {
-		async function getNumber() {
-			const jwt = localStorage.getItem('token');
-			const path = process.env.REACT_APP_API_BASE;
-			let data = await fetch(`${path}users/contact/${uuid}`, {
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${jwt}`,
-				},
-			});
-			let result = await data.json();
+  useEffect(() => {
+    async function getNumber() {
+      const jwt = localStorage.getItem('token');
+      const path = process.env.REACT_APP_API_BASE;
+      let data = await fetch(`${path}users/contact/${uuid}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      let result = await data.json();
 
-			if (result.contactData.performers[0].contact_number) {
-				const userPhoneNumber: number = result.contactData.performers[0].contact_number;
-				setWSLink(`https://wa.me/852${userPhoneNumber}`);
-				console.log(`https://wa.me/852${userPhoneNumber}`);
-			}
-		}
-		getNumber();
-	}, [uuid]);
+      if (result.contactData.performers[0].contact_number) {
+        const userPhoneNumber: number = result.contactData.performers[0].contact_number;
+        setWSLink(`https://wa.me/852${userPhoneNumber}`);
+        console.log(`https://wa.me/852${userPhoneNumber}`);
+      }
+    }
+    getNumber();
+  }, [uuid]);
 
-	let role: Role = Role.Performer;
-	if (resp?.data && resp.data.identity === 'client') {
-		const userInfo = resp.data as IndividualClientsInfo | CorporateClientsInfo;
-		role = userInfo.client_type === 'individual' ? Role.Individual : Role.Corporate;
-	}
-	const uuidFromState = useRootSelector((state) => state.auth.uuid);
+  let role: Role = Role.Performer;
+  if (resp?.data && resp.data.identity === 'client') {
+    const userInfo = resp.data as IndividualClientsInfo | CorporateClientsInfo;
+    role = userInfo.client_type === 'individual' ? Role.Individual : Role.Corporate;
+  }
+  const uuidFromState = useRootSelector((state) => state.auth.uuid);
 
-	if (isLoading) {
-		return <div>isLoading...</div>;
-	}
-	if (error) {
-		return (
-			<div>
-				<div>Who are You?</div>
-			</div>
-		);
-	}
+  if (isLoading) {
+    return <div>isLoading...</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        <div>Who are You?</div>
+      </div>
+    );
+  }
 
-	const returnPerformer = () => {
-		if (!resp) return;
-		const userInfo = resp.data as PerformanceInfo;
-		return <PersonalInfo pageUUID={uuid!} performanceInfo={userInfo} />;
-	};
-	const returnCorporate = () => {
-		if (!resp) return;
-		const userInfo = resp.data as CorporateClientsInfo;
-		return <PersonalInfo pageUUID={uuid!} corporateClientsInfo={userInfo} />;
-	};
-	const returnIndividual = () => {
-		if (!resp) return;
-		const userInfo = resp.data as IndividualClientsInfo;
-		return <PersonalInfo pageUUID={uuid!} individualClientsInfo={userInfo} />;
-	};
+  const returnPerformer = () => {
+    if (!resp) return;
+    const userInfo = resp.data as PerformanceInfo;
+    return <PersonalInfo pageUUID={uuid!} performanceInfo={userInfo} />;
+  };
+  const returnCorporate = () => {
+    if (!resp) return;
+    const userInfo = resp.data as CorporateClientsInfo;
+    return <PersonalInfo pageUUID={uuid!} corporateClientsInfo={userInfo} />;
+  };
+  const returnIndividual = () => {
+    if (!resp) return;
+    const userInfo = resp.data as IndividualClientsInfo;
+    return <PersonalInfo pageUUID={uuid!} individualClientsInfo={userInfo} />;
+  };
 
-	return (
-		<div className='Body'>
-			<div className='Title'>About</div>
-			<div className='About-ButtonGroup'></div>
-			{resp && role === Role.Performer && <div>{returnPerformer()}</div>}
+  return (
+    <div className='Body'>
+      <div className='Title'>About</div>
+      <div className='About-ButtonGroup'></div>
+      {resp && role === Role.Performer && <div>{returnPerformer()}</div>}
 
-			{resp && role === Role.Corporate && <div>{returnCorporate()}</div>}
+      {resp && role === Role.Corporate && <div>{returnCorporate()}</div>}
 
-			{resp && role === Role.Individual && <div>{returnIndividual()}</div>}
-		</div>
-	);
+      {resp && role === Role.Individual && <div>{returnIndividual()}</div>}
+    </div>
+  );
 }
 
 export default About;
