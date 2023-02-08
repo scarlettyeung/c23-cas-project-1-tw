75b7811c3d36b18d88f792cdd52691d067c702bf
Ken Chan <kenchan6694@gmail.com>
2023-02-08 11:49:11 +0800

    updated error 404

--- project-app/src/pages/NotFound.tsx
+++ project-app/src/pages/NotFound.tsx
@@ -2,44 +2,44 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 
 function NotFound() {
-	const navigate = useNavigate();
-	const [time, setTime] = useState<number>(5);
+  const navigate = useNavigate();
+  const [time, setTime] = useState<number>(5);
 
-	useEffect(() => {
-		const timerToHome = setTimeout(() => {
-			navigate('/');
-		}, 5000);
+  useEffect(() => {
+    const timerToHome = setTimeout(() => {
+      navigate('/');
+    }, 5000);
 
-		return () => {
-			clearTimeout(timerToHome);
-		};
-	}, [navigate]);
+    return () => {
+      clearTimeout(timerToHome);
+    };
+  }, [navigate]);
 
-	useEffect(() => {
-		const timerToCount = setTimeout(() => {
-			setTime(time - 1);
-		}, 1000);
+  useEffect(() => {
+    const timerToCount = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
 
-		return () => {
-			clearTimeout(timerToCount);
-		};
-	}, [time]);
+    return () => {
+      clearTimeout(timerToCount);
+    };
+  }, [time]);
 
-	return (
-		<div>
-			<h1>404 Not Found</h1>
-			<img src='../../public/404.jpeg' alt=''></img>
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <img className="errorImage" src='../../public/404.jpeg' alt=''></img>
 
-			<div>Will Go back at {time}s</div>
-			<button
-				onClick={() => {
-					navigate('/');
-				}}
-			>
-				back to home
-			</button>
-		</div>
-	);
+      <div>Will Go back at {time}s</div>
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        back to home
+      </button>
+    </div>
+  );
 }
 
 export default NotFound;
