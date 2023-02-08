60dbd4288912660cefe51c9c10fb0265d482af86
Ken Chan <kenchan6694@gmail.com>
2023-02-08 14:53:15 +0800

    updated header

--- project-app/src/components/Layout/Header.tsx
+++ project-app/src/components/Layout/Header.tsx
@@ -4,11 +4,11 @@ import Search from './Search';
 import '../../styles/header.css';
 
 function Header() {
-	return (
-		<div className='header-bar'>
-			<Search />
-		</div>
-	);
+  return (
+    <div className='header-bar'>
+      <Search />
+    </div>
+  );
 }
 
 export default Header;
