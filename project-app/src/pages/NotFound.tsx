bde771119dca76b1c44af055972149853f8cca5b
Maple02149 <b20060187@gmail.com>
2023-02-08 10:47:31 +0800

    404page back home function

--- project-app/src/pages/NotFound.tsx
+++ project-app/src/pages/NotFound.tsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+
 function NotFound() {
+	const navigate = useNavigate();
+	const [time, setTime] = useState<number>(5);
+
+	useEffect(() => {
+		const timerToHome = setTimeout(() => {
+			navigate('/');
+			console.log('1231');
+		}, 5000);
+
+		return () => {
+			clearTimeout(timerToHome);
+		};
+	}, [navigate]);
+
+	useEffect(() => {
+		const timerToCount = setTimeout(() => {
+			setTime(time - 1);
+		}, 1000);
+
+		return () => {
+			clearTimeout(timerToCount);
+		};
+	});
+
 	return (
-		<div>
+		<>
 			<h1>404 Not Found</h1>
-		</div>
+			<br></br>
+			<h1>will go home after {time}s</h1>
+
+			<button
+				onClick={() => {
+					navigate('/');
+				}}
+			>
+				back to home
+			</button>
+		</>
 	);
 }
 
