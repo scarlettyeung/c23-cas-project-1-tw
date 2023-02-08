c345aa33cba251a43fe077acb87390f0356712d8
Maple02149 <b20060187@gmail.com>
2023-02-08 18:48:16 +0800

    ready to build

--- project-app/src/components/Layout/Header.tsx
+++ project-app/src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import '../../styles/header.css';
 import { useRootSelector } from '../../redux/store';
 import { useState, useEffect } from 'react';
 import { getAllHashTags } from '../../redux/search';
-import { Group, Select, Badge, Text } from '@mantine/core';
+import { Group, Select } from '@mantine/core';
 import { IconChevronDown } from '@tabler/icons-react';
 import { tagType } from '../../utils/SearchType';
 import { useRootDispatch } from '../../redux/store';
@@ -11,86 +11,86 @@ import { SearchTagType, FetchPerformerType, FetchEventDataType } from '../../uti
 import Logout from '../Logout';
 
 function Header() {
-  ////part of sse
-  // const path = process.env.REACT_APP_API_BASE;
-  // if (typeof EventSource !== 'undefined' && path) {
-  // 	const sse = new EventSource(path, { withCredentials: true });
+	////part of sse
+	// const path = process.env.REACT_APP_API_BASE;
+	// if (typeof EventSource !== 'undefined' && path) {
+	// 	const sse = new EventSource(path, { withCredentials: true });
 
-  // 	sse.onmessage = function (e) {
-  // 		console.log(e.data);
-  // 	};
-  // 	// source.addEventListener(
-  // 	// 	'message',
-  // 	// 	function (e) {
-  // 	// 		console.log(e.data);
-  // 	// 	},
-  // 	// 	false,
-  // 	// );
+	// 	sse.onmessage = function (e) {
+	// 		console.log(e.data);
+	// 	};
+	// 	// source.addEventListener(
+	// 	// 	'message',
+	// 	// 	function (e) {
+	// 	// 		console.log(e.data);
+	// 	// 	},
+	// 	// 	false,
+	// 	// );
 
-  // 	// sse.onmessage = function (event) {
-  // 	// 	const newElement = document.createElement('li');
-  // 	// 	const eventList = document.getElementById('list');
+	// 	// sse.onmessage = function (event) {
+	// 	// 	const newElement = document.createElement('li');
+	// 	// 	const eventList = document.getElementById('list');
 
-  // 	// 	newElement.innerHTML = 'message: ' + event.data;
-  // 	// 	if (newElement && eventList) eventList.appendChild(newElement);
-  // 	// };
-  // } else {
-  // 	alert('browser does not support sse function, message may not be received');
-  // }
-  ////end of sse
+	// 	// 	newElement.innerHTML = 'message: ' + event.data;
+	// 	// 	if (newElement && eventList) eventList.appendChild(newElement);
+	// 	// };
+	// } else {
+	// 	alert('browser does not support sse function, message may not be received');
+	// }
+	////end of sse
 
-  ////part of search function
-  const userName = useRootSelector((state) => state.auth.username);
-  const userIdentity = useRootSelector((state) => state.auth.identity);
-  const [query, setQuery] = useState<string>('performer');
-  ////ent of search function
+	////part of search function
+	const userName = useRootSelector((state) => state.auth.username);
+	// const userIdentity = useRootSelector((state) => state.auth.identity);
+	const [query, setQuery] = useState<string>('performer');
+	////ent of search function
 
-  ////part of data
+	////part of data
 
-  const dispatch = useRootDispatch();
-  useEffect(() => {
-    dispatch(getAllHashTags({ hashTags: query }));
-  }, [dispatch, query]);
-  const hashtagArr = useRootSelector<FetchPerformerType[] | FetchEventDataType[]>(
-    (state) => state.search.hashtagArr,
-  );
-  if (query === SearchTagType.Performer) {
-    hashtagArr as FetchPerformerType[];
-  }
-  if (query === SearchTagType.Event) {
-    hashtagArr as FetchEventDataType[];
-  }
-  ////end of data
+	const dispatch = useRootDispatch();
+	useEffect(() => {
+		dispatch(getAllHashTags({ hashTags: query }));
+	}, [dispatch, query]);
+	const hashtagArr = useRootSelector<FetchPerformerType[] | FetchEventDataType[]>(
+		(state) => state.search.hashtagArr,
+	);
+	if (query === SearchTagType.Performer) {
+		hashtagArr as FetchPerformerType[];
+	}
+	if (query === SearchTagType.Event) {
+		hashtagArr as FetchEventDataType[];
+	}
+	////end of data
 
-  return (
-    <div className='header-bar'>
-      <Group>
-        <p>Hi, {userName}</p>
-      </Group>
-      <Group>
-        <Select
-          className='sbar'
-          rightSection={<IconChevronDown size={14} />}
-          rightSectionWidth={25}
-          placeholder='Pick one'
-          data={tagType}
-          value={query}
-          w={100}
-          maxDropdownHeight={400}
-          onChange={(v) => {
-            if (v) {
-              setQuery(v);
-              dispatch(getAllHashTags({ hashTags: v }));
-            }
-          }}
-        />
-        <Search query={query} hashtagArr={hashtagArr} />
-        <div className='logOut-Btn'>
-          <Logout />
-        </div>
-      </Group>
-    </div>
-  );
+	return (
+		<div className='header-bar'>
+			<Group>
+				<p>Hi, {userName}</p>
+			</Group>
+			<Group>
+				<Select
+					className='sbar'
+					rightSection={<IconChevronDown size={14} />}
+					rightSectionWidth={25}
+					placeholder='Pick one'
+					data={tagType}
+					value={query}
+					w={100}
+					maxDropdownHeight={400}
+					onChange={(v) => {
+						if (v) {
+							setQuery(v);
+							dispatch(getAllHashTags({ hashTags: v }));
+						}
+					}}
+				/>
+				<Search query={query} hashtagArr={hashtagArr} />
+				<div className='logOut-Btn'>
+					<Logout />
+				</div>
+			</Group>
+		</div>
+	);
 }
 
 export default Header;
