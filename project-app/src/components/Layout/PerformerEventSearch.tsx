cca87a449707145ae06d3c416a2d12d207a9bbb0
Maple02149 <b20060187@gmail.com>
2023-02-08 18:41:47 +0800

    fix get undefined icon

--- project-app/src/components/Layout/PerformerEventSearch.tsx
+++ project-app/src/components/Layout/PerformerEventSearch.tsx
@@ -5,84 +5,88 @@ import { IconSearch } from '@tabler/icons-react';
 import '../../index.css';
 const { REACT_APP_IMAGE_BASE } = process.env;
 interface PropsType {
-  data: SpotlightAction[];
+	data: SpotlightAction[];
 }
 
 function SpotlightControl() {
-  return (
-    <Group position='center'>
-      <button className='search-Btn' onClick={() => openSpotlight()}>
-        <IconSearch />
-      </button>
-    </Group>
-  );
+	return (
+		<Group position='center'>
+			<button className='search-Btn' onClick={() => openSpotlight()}>
+				<IconSearch />
+			</button>
+		</Group>
+	);
 }
 const useStyles = createStyles(() => ({
-  action: {
-    position: 'relative',
-    display: 'block',
-    width: '100%',
-    padding: '10px 12px',
-  },
+	action: {
+		position: 'relative',
+		display: 'block',
+		width: '100%',
+		padding: '10px 12px',
+	},
 }));
 
 function CustomAction({
-  action,
-  styles,
-  classNames,
-  hovered,
-  onTrigger,
-  ...others
+	action,
+	styles,
+	classNames,
+	hovered,
+	onTrigger,
+	...others
 }: SpotlightActionProps) {
-  const { classes, cx } = useStyles();
-  console.log(action.title);
-  return (
-    <Button
-      className={(cx(classes.action), 'header__performerEventSearch')}
-      tabIndex={-1}
-      onMouseDown={(event) => event.preventDefault()}
-      onClick={onTrigger}
-      {...others}
-    >
-      <Group noWrap>
-        <Center>
-          <Avatar
-            src={`${REACT_APP_IMAGE_BASE}/${action.icon}`}
-            alt={action.title}
-            key={`s_${action.image}_${action.id}`}
-          />
-        </Center>
+	const { classes, cx } = useStyles();
 
-        <div style={{ flex: 1 }}>
-          <Text>{action.title}</Text>
+	return (
+		<Button
+			className={(cx(classes.action), 'header__performerEventSearch')}
+			tabIndex={-1}
+			onMouseDown={(event) => event.preventDefault()}
+			onClick={onTrigger}
+			{...others}
+		>
+			<Group noWrap>
+				<Center>
+					{action.icon ? (
+						<Avatar
+							src={`${REACT_APP_IMAGE_BASE}/${action.icon}`}
+							alt={action.title}
+							key={`s_${action.image}_${action.id}`}
+						/>
+					) : (
+						<Avatar alt={action.title} key={`s_${action.image}_${action.id}`} />
+					)}
+				</Center>
 
-          {action.description && (
-            <Text color='dimmed' size='xs'>
-              {action.description}
-            </Text>
-          )}
-        </div>
+				<div style={{ flex: 1 }}>
+					<Text>{action.title}</Text>
 
-        {action.new && <Badge>new</Badge>}
-      </Group>
-    </Button>
-  );
+					{action.description && (
+						<Text color='dimmed' size='xs'>
+							{action.description}
+						</Text>
+					)}
+				</div>
+
+				{action.new && <Badge>new</Badge>}
+			</Group>
+		</Button>
+	);
 }
 
 function PerformerEventSearch(props: PropsType) {
-  return (
-    <>
-      <SpotlightProvider
-        actions={props.data}
-        actionComponent={CustomAction}
-        limit={7}
-        searchPlaceholder='Search...'
-        nothingFoundMessage='Nothing found...'
-      >
-        <SpotlightControl />
-      </SpotlightProvider>
-    </>
-  );
+	return (
+		<>
+			<SpotlightProvider
+				actions={props.data}
+				actionComponent={CustomAction}
+				limit={7}
+				searchPlaceholder='Search...'
+				nothingFoundMessage='Nothing found...'
+			>
+				<SpotlightControl />
+			</SpotlightProvider>
+		</>
+	);
 }
 
 export default PerformerEventSearch;
